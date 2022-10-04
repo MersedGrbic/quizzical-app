dcc20aba0bc4179c5744e6f07c71c01b6be30736
MersedGrbic <101355423+MersedGrbic@users.noreply.github.com>
2022-10-04 20:41:16 +0200

    Start

---
+++ quizz/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Intro from './components/Intro'
+function App() {
+  
+
+  return (
+    <div className="App">
+      <Intro/>
+    </div>
+  )
+}
+
+export default App
---
+++ quizz/src/components/Intro.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Intro(){
+    return (
+        <div className="intro">
+            <h1><span>Q</span>uizzical</h1>
+            <div className="introduction">
+            <p>Simple Quizz Made With </p>
+            <p><i class="fa-brands fa-react"></i></p>
+            </div>
+            <button className="start-btn">Start quiz</button>
+        </div>
+    )
+}
