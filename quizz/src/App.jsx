6505083b5e9d828bfeb87d67ee1b6459078bcfcd
MersedGrbic <101355423+MersedGrbic@users.noreply.github.com>
2022-10-23 19:01:28 +0200

    Code reorganization.

--- quizz/src/App.jsx
+++ quizz/src/App.jsx
@@ -1,25 +1,35 @@
 import React, { useState } from 'react'
-import { nanoid } from 'nanoid'
-import { ReactDOM } from 'react'
-import { useEffect } from 'react'
 import './App.css'
 import Intro from './components/Intro'
 import Quizz from './components/Quizz'
 function App() {
   
   const [start,setStart]= React.useState(true)
+  const [quizz,setQuizz]= React.useState([])
+
+React.useEffect(()=>{
+  const fetchData = async()=>{
+  const res = await fetch ('https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple   ')
+  const data = await res.json()
+  setQuizz(data.results)
+  }
+  fetchData()
+},[])
+
+const quizzElements = quizz.map(elem=>{
   
-  
-  function handleClick(){
+  return <Quizz 
+   data={elem}
+  />
+})
+function handleClick(){
     setStart(false)
   }
   
   return (
     <div className="App">
       
-      {start && <Intro handleClick={handleClick}/>}
-      {!start && <Quizz/>}
-      
+      {start ? <Intro handleClick={handleClick}/> : quizzElements }
       
     </div> 
   )
--- quizz/src/components/Quizz.jsx
+++ quizz/src/components/Quizz.jsx
@@ -1,59 +1,34 @@
 import React from "react";
+import Answer from "./Answer"
 
-export default function Quizz(){
-
-    const [quizz,setQuizz] = React.useState([])
-    
-    function decodeHTML(html){
-        let txt= document.createElement('textarea');
-        txt.innerHTML=html;
-        return txt.value
-    }
-
-    function shuffleArray(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }
-    }
-    
-    React.useEffect(()=>{
-        const fetchData = async()=>{
-        const res = await fetch ('https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple   ')
-        const data = await res.json()
-        setQuizz(data.results)
-        }
-        fetchData()
-        
-    },[])
-    
-    const quizzElements = quizz.map(elem=>{
-        
-        const questions = [elem.correct_answer,elem.incorrect_answers[0],elem.incorrect_answers[1],elem.incorrect_answers[2]]
-        shuffleArray(questions)
-        console.log(elem.correct_answer)
-        return (
-            <>
-            <div className="wrapper">
-                <div className="question">{decodeHTML(elem.question)}</div>
-                  <div className="questions-list">
-                    <p>{decodeHTML(questions[0])}</p>
-                    <p>{decodeHTML(questions[1])}</p>
-                    <p>{decodeHTML(questions[2])}</p>
-                    <p>{decodeHTML(questions[3])}</p>
-                  </div>
-            </div>
-            <div className="line"></div>
-            </>
-        )
-    })
-    
-    
-    
-    return (
-        <div className="questions">
-            {quizzElements}
-            {quizz.length>0 && <button className="start-btn">Check Answers</button>}
+export default function Quizz(props){
+ 
+     function decodeHTML(html){
+    let txt= document.createElement('textarea');
+    txt.innerHTML=html;
+    return txt.value
+}
+function shuffleArray(array) {
+   for (let i = array.length - 1; i > 0; i--) {
+       const j = Math.floor(Math.random() * (i + 1));
+       [array[i], array[j]] = [array[j], array[i]];
+   }
+}
+ const answers = [decodeHTML(props.data.correct_answer), decodeHTML(props.data.incorrect_answers[0]),decodeHTML(props.data.incorrect_answers[1]),decodeHTML(props.data.incorrect_answers[2])]
+ shuffleArray(answers)
+ const answersElements= answers.map(ans=>{
+    return <Answer 
+    answer={ans}
+    correctAnswer={(props.data.correct_answer)}
+    on = {true}
+    />
+ })
+ return (
+    <div className="wrapper">
+        <h2>{decodeHTML(props.data.question)}</h2>
+        <div className="answer-list">
+        {answersElements}
         </div>
-    )
+    </div>
+  )
 }
