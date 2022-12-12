46db7f68153a8ff0f24344b4e01910e38645e96b
MersedGrbic <101355423+MersedGrbic@users.noreply.github.com>
2022-12-12 15:44:49 +0100

    Created Quizzical App (#1)
    * Start
    
    * LEARNING !
    
    * Bug fixes and data display
    
    * Code reorganization.
    
    * updated css layout
    
    * Bug fixes
    
    * Bug fixes
    
    * State handling
    
    * Added select options
    
    Added selected options for category and number of questions to get from API. Started working on styling and mobile responsivnes.
    
    * Bug fixes
    
    * deploying
    
    * Moving everything to one folder
    
    * Update package.json
    
    * Bug fix, addes results
    
    Fixed bug that shows asnswers when clicked Check Answers button.
    Quizz now shows Score of a player.
    
    * Refactoring
    
    * Formatted
    
    * vite reinstall
    
    Error while trying to start an app using npm run dev, reinstalling vite .
    
    * Update README.md
    
    * Update README.md

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Intro from "./components/Intro";
+import Quizz from "./components/Quizz";
+import { useState } from "react";
+import { nanoid } from "nanoid";
+
+function App() {
+  const [start, setStart] = useState(true);
+  const [quizzInfo, setQuizzInfo] = useState("");
+  const [category, setCategory] = useState("");
+  const [numOfQuestions, setNumOfQuestions] = useState(5);
+
+  const handleChange = (event) => {
+    setCategory(event.target.value);
+  };
+
+  const handleNumberOfQuestions = (event) => {
+    setNumOfQuestions(event.target.value);
+  };
+
+  const handleClick = () => {
+    setStart((prevState) => !prevState);
+  };
+  console.log("render");
+
+  return (
+    <div className="App">
+      {start ? (
+        <Intro
+          handleChange={handleChange}
+          handleNumberOfQuestions={handleNumberOfQuestions}
+          handleClick={handleClick}
+        />
+      ) : (
+        <Quizz
+          category={category}
+          numOfQuestions={numOfQuestions}
+          handleClick={handleClick}
+        />
+      )}
+    </div>
+  );
+}
+
+export default App;
